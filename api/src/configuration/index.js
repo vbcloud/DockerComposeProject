8e343bbf38a9a69b5928b69ae55bbdab144944c5
vbcloud <vilabrunettecloud@gmail.com>
2025-04-01 18:44:55 -0400

    json fix

--- api/src/configuration/index.js
+++ api/src/configuration/index.js
@@ -1,4 +1,4 @@
 module.exports.port = process.env.PORT || 3000;
 module.exports.host = process.env.HOST || 'localhost';
 module.exports.db = process.env.MONGO_URL;
-
+module.exports.authApiUrl = process.env.AUTH_API_URL
